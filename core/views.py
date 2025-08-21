fb5bf1207c3f6741d7ef7ede329a338c425eef31
LinusBwana <linusbwana49@gmail.com>
2025-08-21 14:40:02 +0300

    Configured the register functionality to enable registering of new users

--- core/views.py
+++ core/views.py
@@ -14,6 +14,40 @@ def Home(request):
     return render(request, 'index.html')
 
 def RegisterView(request):
+
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user_data_has_error = False
+
+        if User.objects.filter(username=username).exists():
+            user_data_has_error = True
+            messages.error(request, 'Username already exists')
+
+        if User.objects.filter(email=email).exists():
+            user_data_has_error = True
+            messages.error(request, 'Email already exists')
+
+        if len(password) < 5:
+            user_data_has_error = True
+            messages.error(request, 'Password must be at least 5 characters')
+
+        if not user_data_has_error:
+            new_user = User.objects.create_user(
+                first_name = first_name,
+                last_name = last_name,
+                email = email,
+                username = username,
+                password = password
+            )
+            messages.success(request, 'Account created. Login now')
+            return redirect('login')
+        return redirect('register')
     return render(request, 'register.html')
 
 def LoginView(request):
